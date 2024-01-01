a5a3ce558002651c0e6e7b444e224de2cb70c695
Ankush <alamba570@gmail.com>
2024-01-01 16:25:04 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Parallax from './components/parallel4/Parallel';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Parallax/>
+      
+    </div>
+  );
+}
+
+export default App;
